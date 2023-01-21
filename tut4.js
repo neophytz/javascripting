5d53cc4c93287d92d3077e06410a8739672e9a3b
NIKHIL GUPTA <nikhilgupta8800@gmail.com>
2023-01-21 22:38:57 +0530

    tutorial 4 completed

---
+++ tut4.js
@@ -0,0 +1,125 @@
+// number (int, float, NaN)
+// strings (char, string, string methods/functions)
+// boolean
+
+// boolean => true false
+
+// building logic -> we need to use boolean
+
+// var num1 = 41; // assignment
+// var num2 = '41'; // assignment
+
+// equality check
+// one depends on data type
+
+    // num1 == num2 => boolean value (either true or false)
+
+// doesn't depends on data type
+
+    //num1 === num2 => boolean value (either t or f but this time it will check for same data type)
+
+// inequality check
+// one depends on data type
+
+    // num1 != num2 => boolean value (either true or false)
+
+// doesn't depends on data type
+
+    //num1 !== num2 => boolean value (either t or f but this time it will check for same data type)
+
+
+
+// greater lesser strictly greater strictly lesser
+
+// num1 < num2 
+// num2 < num1 
+// num1 > num2
+// num2 > num1
+
+// num1 <= / <== num2 
+// num1 >= / >== num2
+
+
+// check if they are same 
+// console.log("Same values")
+// check if they are not same 
+// console.log("Different values")
+
+
+// if, else, else if statements
+
+// if(condition){
+//     // do some stuff
+// }
+// else{
+//     // do some stuff
+// }
+
+var num1 = 49;
+var num2 = 44;
+
+// if(num1 === num2){
+//     console.log("Same values")
+// }
+// else{
+//     console.log("Different values")
+// }
+
+
+
+// if 
+// else if 
+// else if 
+// else if 
+// else if 
+// else if 
+// else if 
+// else 
+
+
+// if else statement
+
+// if (num1 === num2) {
+//     console.log("same values");
+// }
+
+// else if ( num1 < num2 ) {
+//     console.log("num1 less than num2");
+// }
+    
+// else {
+//     console.log("num1 greater than num2");
+// }
+
+
+// switch case statements
+
+
+
+// falsy values
+
+// condition => true or false
+// 1 -> boolean 
+// if(1) {
+// }
+// else
+
+//number => if (num1) {
+// } else {}
+
+// num1===0, NaN => false
+// otherwise +ve -ve => true
+
+num1 = NaN
+
+// num1 and num2
+
+if(num1){
+    console.log("num1 is not zero/NaN")
+}
+else console.log("num1 is zero/NaN")
+
+
+
+// arithmetic in case of number
+// boolean -> boolean algebra
