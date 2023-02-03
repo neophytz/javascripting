3871a70ebafe6a4e21e238532d5e54cc9ed6d4ad
Gupta-Nikhil85 <nikhilgupta8800@gmail.com>
2023-02-03 22:14:01 +0530

    tutorial 11

--- tut10.js
+++ tut10.js
@@ -68,11 +68,11 @@
 
 // for/in loop -> objects;
 
-var obj = {
-    name:"nikhil",
-    batch:"fsdp",
-    age:19
-}
+// var obj = {
+//     name:"nikhil",
+//     batch:"fsdp",
+//     age:19
+// }
 
 // for( key_variable in object_name) {
 //     // code
@@ -127,3 +127,49 @@ var obj = {
 // arr.forEach(oddEven); // it will iterate over array and calls oddeven function
 
 // homework -> read about while and do while loop
+
+// while(condition) {
+//     //code
+// }
+
+// for(exp1; exp2; exp3){
+    // code
+// }
+
+
+// var i = 11;
+// // i =1,3,5,7,9,11
+// while (i<=10) { // true, true
+//     console.log("2 X", i, "=", 2*i); // 2, 6
+//     i=i+2; // i+=2;
+// }
+// console.log(i);
+
+// do {
+//     //code
+// } while (condition)
+
+
+// // for the first iteration, it will not check the condition and will run the loop irrespective of the condition
+// var i = 11;
+// do{
+//     console.log("2 X", i, "=", 2*i); // 2, 6
+//     i=i+2; // i+=2;
+// } while(i<=10)
+
+// var arr = [1,1,1,1,3,4,5];
+
+// set
+
+// var st= new Set(arr);// create a set using array // 1,3,4,5
+// console.log(st.size);// find length of set
+// st.add(14); //1,3,4,5,14
+// st.add(3);  //1,3,4,5,14
+// st.add(14); //1,3,4,5,14
+// st.add(5);  //1,3,4,5,14
+// console.log(st.size);// find length of set
+
+// // 7 existance
+// st.delete(7);
+// console.log(st.has(14));
+// console.log(st.values());
