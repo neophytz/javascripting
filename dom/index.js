c5af3f961bb08aec3b8fb9c2b5ae4322c6689da3
Gupta-Nikhil85 <nikhilgupta8800@gmail.com>
2023-02-05 22:25:44 +0530

    DOM Lecture 2

--- dom/index.js
+++ dom/index.js
@@ -1,57 +1,115 @@
-// // getElementById, getElementsByClassName, getElementByClassName
+// read operations (finding elements) -> querySelector
 
-// var h1Ref = document.getElementById('header');
-// // var btnRef = document.getElementsByTagName('button')[0];
-// // var changeTextBtn = document.getElementById('changeText');
+//getElementsByTagName -> array
 
+// var bodyRef = document.getElementsByTagName('title');
+// bodyRef[0].style.backgroundColor ="red";
+// console.log(bodyRef[0]);
 
-// // function changeToRed () {
-// //     h1Ref.style.color='red';
-// // }
-
-// function changeColor(){
-//     // red -> black
-//     // if(h1Ref.style.color==='red'){
-//     //     h1Ref.style.color='black';
-//     // }
-//     // else h1Ref.style.color='red';
-//     // ternary statements
-//     // condition ? (code 1) : (code2)
-//     // h1Ref.style.color==='red' ? h1Ref.style.color='black' : h1Ref.style.color='red';
-//     h1Ref.style.color = h1Ref.style.color==='red' ? 'black' : 'red';
-// }
-
-// // h1Ref.innerText='goodbye';
+// getElementsByClassName -> array
+// var bodyRef = document.getElementsByClassName('body2');
+// console.log(bodyRef);
 
 
-// function changeText (){
-//     h1Ref.innerText='goodBye';
-// }
-// // changeText();
-// // btnRef.onclick='changeToRed()';
+//getElementById -> single element reference
+// var bodyRef = document.getElementById('bodyId');
+// console.log(bodyRef);
+// bodyRef.style.backgroundColor='red';
 
-// // console.log(btnRef);
-// // ref.style.color='red';
-// // console.log(ref);
+//getElementsByName -> array
+
+// var eleRef = document.getElementsByName('myH1');
+// console.log(eleRef);
 
 
-var couponDivRef = document.getElementById('couponDiv');
-var couponBtnRef = document.getElementById('couponBtn');
+// create operations -> innerHTML, innerText
 
-function revealCoupon(){
-    console.log(couponDivRef.innerHTML);
-    couponBtnRef.innerText = !couponDivRef.innerHTML ? 'Click me to hide coupons.' : 'Click me to reveal coupons.';
-    couponDivRef.innerHTML = couponDivRef.innerHTML==='' ? '<p>Hello, you receive new coupon.</p> <p>your coupon code is yts50.</p>' : '';
-    // falsy values
-    // if(couponDivRef.innerHTML===""){
-    //     couponDivRef.innerHTML = '<p>Hello, you receive new coupon.</p> <p>your coupon code is yts50.</p>';
-    //     couponBtnRef.innerText = 'Click me to hide coupons.'
-    // }else{
-    //     couponDivRef.innerHTML = '';
-    //     couponBtnRef.innerText = 'Click me to reveal coupons.'
-    // }
+var imageRef = document.createElement('img');
+imageRef.setAttribute('src', '../image.jpeg');
+
+var bodyRef = document.getElementById('bodyId');
+bodyRef.appendChild(imageRef);
+
+console.log(imageRef);
+
+// check current image kaunsi hai. -> if else
+// it will change the image accordingly. -> setAttribute()
+
+// console.log(imageRef.src)
+
+function changeImage () {
+    if(imageRef.src === 'http://127.0.0.1:5500/image.jpeg'){
+        imageRef.setAttribute('src', '../image2.jpeg');
+    }
+    else{
+        imageRef.setAttribute('src', '../image.jpeg');
+    }
 }
 
+
+//homework -> implement button creation with the help of javascript.
+
+
+
+
+
+
+
+// // // getElementById, getElementsByClassName, getElementByClassName
+
+// // var h1Ref = document.getElementById('header');
+// // // var btnRef = document.getElementsByTagName('button')[0];
+// // // var changeTextBtn = document.getElementById('changeText');
+
+
+// // // function changeToRed () {
+// // //     h1Ref.style.color='red';
+// // // }
+
+// // function changeColor(){
+// //     // red -> black
+// //     // if(h1Ref.style.color==='red'){
+// //     //     h1Ref.style.color='black';
+// //     // }
+// //     // else h1Ref.style.color='red';
+// //     // ternary statements
+// //     // condition ? (code 1) : (code2)
+// //     // h1Ref.style.color==='red' ? h1Ref.style.color='black' : h1Ref.style.color='red';
+// //     h1Ref.style.color = h1Ref.style.color==='red' ? 'black' : 'red';
+// // }
+
+// // // h1Ref.innerText='goodbye';
+
+
+// // function changeText (){
+// //     h1Ref.innerText='goodBye';
+// // }
+// // // changeText();
+// // // btnRef.onclick='changeToRed()';
+
+// // // console.log(btnRef);
+// // // ref.style.color='red';
+// // // console.log(ref);
+
+
+// var couponDivRef = document.getElementById('couponDiv');
+// var couponBtnRef = document.getElementById('couponBtn');
+
+// function revealCoupon(){
+//     console.log(couponDivRef.innerHTML);
+//     couponBtnRef.innerText = !couponDivRef.innerHTML ? 'Click me to hide coupons.' : 'Click me to reveal coupons.';
+//     couponDivRef.innerHTML = couponDivRef.innerHTML==='' ? '<p>Hello, you receive new coupon.</p> <p>your coupon code is yts50.</p>' : '';
+//     // falsy values
+//     // if(couponDivRef.innerHTML===""){
+//     //     couponDivRef.innerHTML = '<p>Hello, you receive new coupon.</p> <p>your coupon code is yts50.</p>';
+//     //     couponBtnRef.innerText = 'Click me to hide coupons.'
+//     // }else{
+//     //     couponDivRef.innerHTML = '';
+//     //     couponBtnRef.innerText = 'Click me to reveal coupons.'
+//     // }
+// }
+
+// // homework :- read about getElement...../getElements.....
 
 
 
